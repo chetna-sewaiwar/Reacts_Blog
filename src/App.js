12d7a820bdd1caf258fe54cab0ee3245b4185656
chetna-sewaiwar <chetnasewaiwar@gmail.com>
2023-06-12 00:23:28 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Navbar from './Navbar/Navbar';
+
+import './App.css';
+import RouterFile from './Components/RouterFile';
+import Footer from './Components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      {/* <Home/> */}
+      <RouterFile/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='Footer'>
+            © 2023 Aiswarya Regi
+    </div>
+  )
+}
+
+export default Footer
